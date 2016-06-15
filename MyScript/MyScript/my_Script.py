4e3970e63dcb7118d38eb9ce8017f19eb13cd160
김병민 <acuer@kpu.ac.kr>
2016-06-16 05:45:55 +0900

    구글 지도 연동

--- MyScript/MyScript/my_Script.py
+++ MyScript/MyScript/my_Script.py
@@ -6,7 +6,6 @@ import os
 import webbrowser
 import mimetypes
 import mysmtplib
-import re
 
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
@@ -19,23 +18,30 @@ class Data:
 
 
     def PrintMenu(self):
+        print("\n")
         print(" 대구시 맛집정보 ")
-        print(" 1. ㅇㄹㅇㄻㄴㄹ" )
-        print(" 2. 전체 출력")
+        print("\n")
+        print(" 1. 음식 분류별 검색" )
+        print(" 2. 구군별 검색")
         print(" 3. 이메일 보내기")
         print(" 4. 다음으로 검색")
-        print(" 8. 종료")
+        print(" 5. 위치 검색")
+        print(" 0. 종료")
+        print("\n")
 
     def Choose(self, menu):
+        os.system('cls')
         if menu == '1':
-            self.Fileset()
-        elif menu == '2':
             self.PrintWhat()
+        elif menu == '2':
+            self.PrintWhere()
         elif menu == '3':
             self.SendEmail()
         elif menu == '4':
             self.SearchDaum()
-        elif menu == '8':
+        elif menu == '5':
+            self.SearchMap()
+        elif menu == '0':
             self.Exit()
         else:
             print("다시 입력하세요")
@@ -43,37 +49,59 @@ class Data:
     def Exit(self):
         exit()
 
-
-    def Fileset(self):
-        data = urllib.request.urlopen(self.url).read()
-        f = open("foodrtrt.xml", "wb")
-        f.write(data)
-        f.close()
-
     def PrintWhat(self):
-        Where = str(input('\n'+'음식 분류(한식, 중식, 양식, 일식, 세계요리) : '))
+        os.system('cls')
+        What = str(input('\n'+'음식 분류(한식, 중식, 양식, 일식, 세계요리) : '))
         root = ET.parse("foodrtrt.xml").getroot()
         for a in root.findall('row'):
-            print('*=' * 55)
-            print("구군 분류       : ",a.findtext('GNG_CS'))
-            print("근처 버스정류장 : ",a.findtext('BUS'))
-            print("업소명          : ",a.findtext('BZ_NM'))
-            print("전화번호        : ",a.findtext('TLNO'))
-            print("영업시간        : ",a.findtext('MBZ_HR'))
-            #print("메뉴            : ",a.findtext('MNU'))
-            #print("간단 설명       : ",a.findtext('SMPL_DESC'))
-            print("지하철          : ", a.findtext('SBW'))
-            print("음식분류        : ", a.findtext('FD_CS'))
-            print('*=' * 55)
-            print('\n')
+            if(str(a.findtext('FD_CS')) == What):
+                print('*=' * 55)
+                print("구군 분류       : ",a.findtext('GNG_CS'))
+                print("근처 버스정류장 : ",a.findtext('BUS'))
+                print("업소명          : ",a.findtext('BZ_NM'))
+                print("전화번호        : ",a.findtext('TLNO'))
+                print("영업시간        : ",a.findtext('MBZ_HR'))
+                #print("메뉴            : ",a.findtext('MNU'))
+                #print("간단 설명       : ",a.findtext('SMPL_DESC'))
+                print("지하철          : ", a.findtext('SBW'))
+                print("음식분류        : ", a.findtext('FD_CS'))
+                print('*=' * 55)
+                print('\n')
+
+    def PrintWhere(self):
+        os.system('cls')
+        Where = str(input(' 구군 입력(서구, 북구, 수성구, 중구) : ' ))
+        root = ET.parse("foodrtrt.xml").getroot()
+        for a in root.findall('row'):
+            if(str(a.findtext('GNG_CS')) == Where):
+                print('*=' * 55)
+                print("구군 분류       : ",a.findtext('GNG_CS'))
+                print("근처 버스정류장 : ",a.findtext('BUS'))
+                print("업소명          : ",a.findtext('BZ_NM'))
+                print("전화번호        : ",a.findtext('TLNO'))
+                print("영업시간        : ",a.findtext('MBZ_HR'))
+                #print("메뉴            : ",a.findtext('MNU'))
+                #print("간단 설명       : ",a.findtext('SMPL_DESC'))
+                print("지하철          : ", a.findtext('SBW'))
+                print("음식분류        : ", a.findtext('FD_CS'))
+                print('*=' * 55)
+                print('\n')
+
+
+    def SearchMap(self):
+        os.system('cls')
+        MAP = str(input("검색어를 입력하세요 : "))
+        url ="https://www.google.co.kr/maps/place/"+MAP
+        webbrowser.open_new(url)
 
     def SearchDaum(self):
+        os.system('cls')
         SFW = str(input("검색어를 입력하세요 : "))
         url ="http://search.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&q="+SFW
         webbrowser.open_new(url)
             
     def SendEmail(self):
-
+        os.system('cls')
         #global value
         host = "smtp.gmail.com" # Gmail STMP 서버 주소.
         port = "587"
@@ -97,6 +125,7 @@ class Data:
         msg.attach(HtmlPart)
         print("\n")
         print("******메일을 보내는 중입니다******")
+        print("\n")
         # 메일을 발송한다.
         s = mysmtplib.MySMTP(host,port)
         #s.set_debuglevel(1)        # 디버깅이 필요할 경우 주석을 푼다.
@@ -105,6 +134,7 @@ class Data:
         s.ehlo()
         s.login("acuer93@gmail.com","roslal12")
         s.sendmail(senderAddr , [recipientAddr], msg.as_string())
+        print("******메일 전송 완료******")
         s.close()
 
 
@@ -120,6 +150,7 @@ def main():
         data.PrintMenu()
         Keyvalue = str(input('메뉴 선택 : '))
         data.Choose(Keyvalue)
+
         
 
 if __name__ == "__main__":
